eac879cc44b267f2c2db624d9adbf5f5d183e708
Alessio <ac.calanchini@gmail.com>
2025-08-24 23:28:20 +0200

    configurazion dei componenti per un progetto Next.js. Inclusi: .gitignore, eslint.config.mjs, next.config.ts, package.json, e vari componenti UI. Implementazione di Prisma per la gestione del database e creazione di file di note per l'uso di Git e Prisma. Aggiunti anche file SVG e CSS globali.

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+
+
+const page =  () => {
+
+
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+  
+      <h1>hello</h1>
+     
+    </div>
+  )
+}
+
+export default page
